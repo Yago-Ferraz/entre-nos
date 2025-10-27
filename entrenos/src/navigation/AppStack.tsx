ff262ce622873bf835b8e4b43b1019a6f4d7aa39
yago-ferraz <yagomouraferraz100@gmail.com>
2025-10-27 17:41:22 -0300

    adicionando outras abas da bottom bar

--- entrenos/src/navigation/AppStack.tsx
+++ entrenos/src/navigation/AppStack.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons, MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
-import HomeScreen from "../views/home/HomeScreen";
 import HomeStack from "./bottombarstacks/HomeStack";
+import LojaStack from "./bottombarstacks/LojaStack";
+import MensagensStacks from "./bottombarstacks/MensagensStacks";
+import SettingsStack from "./bottombarstacks/SettingsStack";
+
+import { cor_primaria, cor_secundaria,cor_terciaria, cor_backgroud} from "../global";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,12 +16,12 @@ export default function AppTabs() {
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: "white",
-          borderTopColor: "#ccc",
+          backgroundColor: cor_primaria,
+          borderTopColor: cor_backgroud,
           borderTopWidth: 1,
         },
-        tabBarActiveTintColor: "#00a86b",
-        tabBarInactiveTintColor: "gray",
+        tabBarActiveTintColor: cor_secundaria,
+        tabBarInactiveTintColor: cor_backgroud,
         tabBarIcon: ({ color, size }) => {
           // render direto no JSX
           if (route.name === "Home") {
@@ -35,6 +39,9 @@ export default function AppTabs() {
       })}
     >
       <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Loja" component={LojaStack} />
+      <Tab.Screen name="Mensagens" component={MensagensStacks} />
+      <Tab.Screen name="Settings" component={SettingsStack} />
 
     </Tab.Navigator>
   );
---
+++ entrenos/src/navigation/bottombarstacks/SettingsStack.tsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../../views/home/HomeScreen";
+import ProdutoScreem from "../../views/home/produtos/viewproduto";
+import Createproduto from "../../views/home/produtos/createproduto";
+
+const Stack = createNativeStackNavigator();
+
+export default function settingsstack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="HomeMain" component={HomeScreen} />
+      <Stack.Screen name="ProdutoScreem" component={ProdutoScreem} />
+      <Stack.Screen name="Createproduto" component={Createproduto} />
+
+    </Stack.Navigator>
+  );
+}
